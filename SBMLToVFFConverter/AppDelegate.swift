af5ecebd67f03e7bfd6bac216ea2c6bd8b856ffd
Jeffrey Varner <jdvarner@purdue.edu>
2015-12-16 15:51:31 -0500

    Initial Commit

---
+++ SBMLToVFFConverter/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SBMLToVFFConverter
+//
+//  Created by Jeffrey Varner on 12/16/15.
+//  Copyright © 2015 Varnerlab. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SBMLToVFFConverter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SBMLToVFFConverter
+//
+//  Created by Jeffrey Varner on 12/16/15.
+//  Copyright © 2015 Varnerlab. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
